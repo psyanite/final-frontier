e4f17d01f56f404d10e53a455bc666e7ea557d8f
Unknown <psyanite@gmail.com>
2018-07-16 12:36:03 +1000

    Implement config

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+import { Constants } from 'expo';
+
+const ENV = {
+  dev: {
+    toasterUrl: 'http://192.168.1.8:3000'
+  },
+  stage: {
+    toasterUrl: 'https://staging.burntoast.com'
+  },
+  prod: {
+    toasterUrl: 'https://burntoast.com'
+  }
+};
+
+function getEnvVars(env = '') {
+  if (env === null || env === undefined || env === '') return ENV.dev;
+  if (env.indexOf('dev') !== -1) return ENV.dev;
+  if (env.indexOf('stage:') !== -1) return ENV.stage;
+  if (env.indexOf('prod') !== -1) return ENV.prod;
+}
+
+export default getEnvVars(Constants.manifest.description);
