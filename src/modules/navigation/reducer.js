fd616c5f69d158d072c161f16f0e9ee4d133e984
Unknown <psyanite@gmail.com>
2018-07-19 17:11:24 +1000

    Refactor navigation reducer

--- src/modules/navigation/reducer.js
+++ src/modules/navigation/reducer.js
@@ -1,12 +1,15 @@
 import { NavigationActions } from 'react-navigation';
+import { createNavigationReducer } from 'react-navigation-redux-helpers';
 import AppNavigator from '../../components/navigation/AppNavigator';
 
 // const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Home'));
-const initialState = AppNavigator.router.getStateForAction(NavigationActions.init());
+// const initialState = AppNavigator.router.getStateForAction(NavigationActions.init());
 
-export default (state = initialState, action) => {
-  const nextState = AppNavigator.router.getStateForAction(action, state);
+// export default (state = initialState, action) => {
+//   const nextState = AppNavigator.router.getStateForAction(action, state);
 
   // Simply return the original `state` if `nextState` is null or undefined.
-  return nextState || state;
-};
+  // return nextState || state;
+// };
+
+export default createNavigationReducer(AppNavigator)
