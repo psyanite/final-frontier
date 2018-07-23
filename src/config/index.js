a054da1b92a987ca517bad731e38698a5eda86ba
psyanite <psyanite@gmail.com>
2018-07-23 13:16:53 +1000

    Rename environment variables

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,22 @@
 import { Constants } from 'expo';
 
 const ENV = {
-  dev: {
+  development: {
     toasterUrl: 'http://192.168.1.10:3000'
   },
   stage: {
-    toasterUrl: 'https://staging.burntoast.com'
+    toasterUrl: 'https://burntoast.herokuapp.com'
   },
-  prod: {
-    toasterUrl: 'https://burntoast.com'
+  production: {
+    toasterUrl: 'https://burntoast.herokuapp.com'
   }
 };
 
 function getEnvVars(env = '') {
   if (env === null || env === undefined || env === '') return ENV.dev;
-  if (env.indexOf('dev') !== -1) return ENV.dev;
-  if (env.indexOf('stage:') !== -1) return ENV.stage;
-  if (env.indexOf('prod') !== -1) return ENV.prod;
+  if (env.indexOf('development') !== -1) return ENV.development;
+  if (env.indexOf('stage') !== -1) return ENV.stage;
+  if (env.indexOf('production') !== -1) return ENV.production;
 }
 
 export default getEnvVars(Constants.manifest.extra.env);
