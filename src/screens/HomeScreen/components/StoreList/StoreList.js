0b09a5e8162efff7282905e0af0d918dd4411aad
Unknown <psyanite@gmail.com>
2018-07-24 12:30:20 +1000

    Refactor store list view into grid view

--- src/screens/HomeScreen/components/StoreList/StoreList.js
+++ src/screens/HomeScreen/components/StoreList/StoreList.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { List } from 'react-native-elements';
 
 import StoreListItem from './StoreListItem';
 
 import Colors from '../../../../styles/constants/ColorConstants';
+import LayoutConstants from '../../../../styles/constants/LayoutConstants';
 
 export default class StoreList extends Component {
   state = {
     ingredientsInput: 'Banana'
   };
 
-  renderSeparator = () => (
-    <View
-      style={{
-        height: 12,
-        width: '100%',
-        backgroundColor: Colors.bodyBackgroundColor,
-      }}
-    />
-  );
+  renderSeparator = () => <View style={{ flex: 1, height: LayoutConstants.margins.m }} />;
 
   renderStore = ({ item }) => (
     <StoreListItem
@@ -30,17 +23,16 @@ export default class StoreList extends Component {
 
   render() {
     return (
-      <View>
-        <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
-          <FlatList
-            data={this.props.stores}
-            renderItem={this.renderStore}
-            keyExtractor={item => item.id.toString()}
-            ItemSeparatorComponent={this.renderSeparator}
-          />
-        </List>
+      <View style={{ marginTop: LayoutConstants.margins.s }}>
+        <FlatList
+          numColumns={2}
+          data={this.props.stores}
+          renderItem={this.renderStore}
+          keyExtractor={item => item.id.toString()}
+          ItemSeparatorComponent={this.renderSeparator}
+        />
       </View>
     );
   }
-
 }
+
--- src/screens/HomeScreen/components/StoreList/StoreListItem.js
+++ src/screens/HomeScreen/components/StoreList/StoreListItem.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import CustomIcon, { Names as CustomIcons } from '../../../../components/common/icons/CustomIcon';
 import LayoutConstants from '../../../../styles/constants/LayoutConstants';
-import GalleryThumbs from '../../../../components/common/gallery/GalleryThumbs';
 
 export default class StoreListItem extends Component {
 
@@ -10,94 +9,50 @@ export default class StoreListItem extends Component {
     this.props.navigateToStore(this.props.store);
   };
 
-  randomPhotos = () => {
-    let rng = Math.floor(Math.random() * 3);
-    const images = [];
-    if (rng === 1) {
-      rng = 5;
-      for (i = 0; i < rng; i++) {
-        images.push({ id: i + 1, photo: this.randomImage() });
-      }
-    }
-    return images;
-  };
-
-  randomImage = () => {
-    const images = [
-      'https://i.imgur.com/r6tqLf9.jpg',
-      'https://i.imgur.com/9K3icV0.jpg',
-      'https://i.imgur.com/8RZOOVo.jpg',
-      'https://b.zmtcdn.com/data/pictures/chains/6/16562676/a940c87e002969862e518cbf196c0017.jpg',
-      'https://b.zmtcdn.com/data/res_imagery/16564570_RESTAURANT_058971a49fafe87b9c772331b251b1fa.jpg',
-      'https://b.zmtcdn.com/data/res_imagery/16566109_RESTAURANT_2b839d1d4dc4319e53bfd12373277d7e.jpg',
-      'https://b.zmtcdn.com/data/reviews_photos/15a/f7279f7dd36cbd4ee3094f618df5c15a_1496048929.jpg',
-      'https://b.zmtcdn.com/data/res_imagery/16557552_CHAIN_37949bc01ae3dd8b992db428a9c35581.jpg',
-      'https://b.zmtcdn.com/data/res_imagery/16566176_RESTAURANT_bdf8b31d9ee9fe53ed91fc17208c0893_c.jpg',
-      'https://b.zmtcdn.com/data/res_imagery/16566248_CHAIN_491a164e22b19761f36df7c833a62e49_c.jpg',
-      'https://b.zmtcdn.com/data/res_imagery/16559171_RESTAURANT_619e090fe7c62a879ce0179d5fedd69f_c.jpg',
-      'https://b.zmtcdn.com/data/res_imagery/16557237_RESTAURANT_1bddec09c720a87210b64e26104f3066_c.jpg'
-    ];
-    const rng = Math.floor(Math.random() * images.length);
-    return images[rng];
-  };
-
   render() {
     const store = this.props.store;
     const view = Object.assign({}, store);
-    const photos = this.randomPhotos();
-    view.location = `${store.suburb.name}, ${store.suburb.city.name}`;
-    if (store.location) {
-      view.location = `${store.location.name}, ${view.location}`;
-    }
 
-    view.cuisines = '';
-    if (store.cuisines) {
-      view.cuisines = store.cuisines.map(cuisine => cuisine.name).join(', ');
-    }
+    view.location = store.location ? store.location.name : store.suburb.name;
+
+    view.cuisines = store.cuisines[0].name;
+
+      // {/*<TouchableOpacity style={styles.wrap} onPress={this.handleOnPress} activeOpacity={1.0}>*/}
+      // </TouchableOpacity>
 
     return (
-      <TouchableOpacity onPress={this.handleOnPress} activeOpacity={1.0}>
-        <View key={view.id} style={styles.wrap}>
-          <View style={styles.header}>
-            <Image
-              style={styles.coverImage}
-              source={{ uri: view.cover_image }}
-            />
-            <View style={styles.deets}>
-              <Text style={styles.name}>{view.name}</Text>
-              <Text style={styles.location}>{view.location}</Text>
-              <Text style={styles.cuisines} numberOfLines={1} ellipsizeMode={'tail'}>{view.cuisines}</Text>
-              <View style={styles.ratings}>
-                <View style={styles.ratingIcon}>
-                  <Text style={styles.ratingCount}>23</Text>
-                  <View style={styles.ratingIcon}>
-                    <CustomIcon name={CustomIcons.BreadHeart} width={24} height={24} />
-                  </View>
-                </View>
-                <View style={styles.ratingIcon}>
-                  <Text style={styles.ratingCount}>15</Text>
-                  <View style={styles.ratingIcon}>
-                    <CustomIcon name={CustomIcons.BreadOkay} width={24} height={24} />
-                  </View>
-                </View>
-                <View style={styles.ratingIcon}>
-                  <Text style={styles.ratingCount}>2</Text>
-                  <View style={styles.ratingIcon}>
-                    <CustomIcon name={CustomIcons.BreadCross} width={24} height={24} />
-                  </View>
-                </View>
-              </View>
-            </View>
-          </View>
-
-          {photos.length > 0 && (
-            <View style={styles.gallery}>
-              <GalleryThumbs images={photos} />
-            </View>
-          )}
-
+      <View style={styles.wrap} key={view.id}>
+        <Image
+          resizeMode='cover'
+          style={styles.coverImage}
+          source={{ uri: view.cover_image }}
+        />
+        <View style={styles.deets}>
+          <Text style={styles.name}>{view.name}</Text>
+          <Text style={styles.location}>{view.location}</Text>
+          <Text style={styles.cuisines} numberOfLines={1} ellipsizeMode={'tail'}>{view.cuisines}</Text>
+          {/*<View style={styles.ratings}>*/}
+          {/*<View style={styles.ratingIcon}>*/}
+          {/*<Text style={styles.ratingCount}>23</Text>*/}
+          {/*<View style={styles.ratingIcon}>*/}
+          {/*<CustomIcon name={CustomIcons.BreadHeart} width={24} height={24} />*/}
+          {/*</View>*/}
+          {/*</View>*/}
+          {/*<View style={styles.ratingIcon}>*/}
+          {/*<Text style={styles.ratingCount}>15</Text>*/}
+          {/*<View style={styles.ratingIcon}>*/}
+          {/*<CustomIcon name={CustomIcons.BreadOkay} width={24} height={24} />*/}
+          {/*</View>*/}
+          {/*</View>*/}
+          {/*<View style={styles.ratingIcon}>*/}
+          {/*<Text style={styles.ratingCount}>2</Text>*/}
+          {/*<View style={styles.ratingIcon}>*/}
+          {/*<CustomIcon name={CustomIcons.BreadCross} width={24} height={24} />*/}
+          {/*</View>*/}
+          {/*</View>*/}
+          {/*</View>*/}
         </View>
-      </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -105,26 +60,22 @@ export default class StoreListItem extends Component {
 const styles = StyleSheet.create({
   wrap: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: LayoutConstants.margins.s,
-    marginBottom: LayoutConstants.margins.s
+    marginLeft: 1,
+    marginRight: 1,
   },
   header: {
     flex: 1,
     flexDirection: 'row',
-    marginRight: LayoutConstants.margins.m,
-    marginLeft: LayoutConstants.margins.m
   },
   coverImage: {
-    width: 100,
-    height: 100,
-    marginRight: 10,
-    borderRadius: 3,
+    height: 130,
   },
 
   deets: {
     flexDirection: 'column',
     flex: 1,
+    padding: LayoutConstants.margins.s,
+    paddingTop: LayoutConstants.margins.xs,
   },
 
   name: {
@@ -160,10 +111,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#9b4e20',
     marginRight: 4,
-  },
-
-  gallery: {
-    marginLeft: LayoutConstants.margins.m,
-    marginTop: LayoutConstants.margins.xs,
   },
 });
